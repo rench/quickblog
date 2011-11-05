778234626686b634caf54bbce256236fb8a5b271
leizongmin <leizongmin@gmail.com>
2011-11-05 11:37:41 +0800

    使用HTML5标签

--- init/start.js
+++ init/start.js
@@ -13,17 +13,17 @@ global.auth = {
 }
 
 // 配置MongoDB连接
-var db_url = exports.db_url = "127.0.0.1:27017/mini-blog";
+var db_url = exports.db_url = "127.0.0.1:27017/quickweb-docs";
 var db = global.db = mongo.db(db_url);
 // 文章集合
 global.article = db.collection('article');
 
 // RSS输出
 global.rss = {
-	path:			'http://pc.ucdok.com/',		// 网站地址
-	title:			'quickblog演示',			// RSS标题
-	description:	'quickblog文章更新'			// 简介
+	path:			'http://quick.cnodejs.net/',	// 网站地址
+	title:			'QuickWeb文档',					// RSS标题
+	description:	'QuickWeb应用实例、更新'		// 简介
 }
 
 // Etag版本号，当修改了系统模板时，通过修改Etag版本号可以使以前的缓存失效
-global.etag = '20111103';
+global.etag = '20111103-3';
